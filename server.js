f3e25a6e3139d8b42a6f82df757986958ca0568c
= <=>
2024-09-23 15:58:02 -0700

    update detail page

--- server.js
+++ server.js
@@ -12,8 +12,20 @@ app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
 
-app.use("/category", categoryRouter);
-app.use("/fundraiser", fundraiserRouter);
+app.use("api/categories", categoryRouter);
+app.use("api/fundraisers", fundraiserRouter);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/search", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "search.html"));
+});
+
+app.get("/fundraiser", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "details.html"));
+});
 
 app.use("*", (req, res) => {
     res.status(404).send();
